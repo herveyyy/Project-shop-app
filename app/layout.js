f53939357c160cc2778b8af6c51b222438047b54
herveyyy <captainhervey@gmail.com>
2024-05-24 09:27:54 +0800

    themeprovider remove

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "../components/ClientSide.jsx";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,10 +9,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ThemeProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ThemeProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
